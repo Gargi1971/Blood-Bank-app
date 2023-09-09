fd33991bff673a89fe4b3b4eb10dce6e7f3e7fb0
Gargi Singh <gargi_singh@icloud.com>
2023-09-09 16:33:42 +0530

    layout

---
+++ client/src/components/DefaultLayout.js
@@ -0,0 +1,88 @@
+import React, { useState } from 'react';
+import {Link} from 'react-router-dom';
+import "./DefaultLayout.css";
+import {
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+} from '@ant-design/icons';
+import { Layout, Menu, Button, theme } from 'antd';
+const { Header, Sider, Content } = Layout;
+const DefaultLayout = () => {
+  const [collapsed, setCollapsed] = useState(false);
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+  return (
+    <Layout>
+      <Sider trigger={null} collapsible collapsed={collapsed}>
+        <div className="demo-logo-vertical" >
+            <h1 className='text-center text-light font-wight-bold mt-2'>CashAide</h1>
+        </div>
+        <Menu>
+          theme="dark"
+          mode="inline"
+          defaultSelectedKeys={['1']}
+          items={[
+            {
+              key: '1',
+              icon: <UserOutlined />,
+              label: 'nav 1',
+            },
+            {
+              key: '2',
+              icon: <VideoCameraOutlined />,
+              label: 'nav 2',
+            },
+            {
+              key: '3',
+              icon: <UploadOutlined />,
+              label: 'nav 3',
+            },
+            {
+                key: '4',
+                icon: <UploadOutlined />,
+                label: 'nav 3',
+            },
+          ]}
+          <Menu.Item key={1} icon={<UserOutlined />}>
+            <Link to='/'>Home</Link>
+          </Menu.Item>
+        </Menu>
+
+      </Sider>
+      <Layout>
+        <Header
+          style={{
+            padding: 0,
+            background: colorBgContainer,
+          }}
+        >
+          <Button
+            type="text"
+            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+            onClick={() => setCollapsed(!collapsed)}
+            style={{
+              fontSize: '16px',
+              width: 64,
+              height: 64,
+            }}
+          />
+        </Header>
+        <Content
+          style={{
+            margin: '24px 16px',
+            padding: 24,
+            minHeight: 280,
+            background: colorBgContainer,
+          }}
+        >
+          Content
+        </Content>
+      </Layout>
+    </Layout>
+  );
+};
+export default DefaultLayout;
---
+++ client/src/pages/Homepage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import DefaultLayout from '../components/DefaultLayout';
+ 
+const Hoempage = () => {
+  return (
+    <div>
+        <DefaultLayout></DefaultLayout>
+    </div>
+  )
+}
+
+export default Hoempage;
